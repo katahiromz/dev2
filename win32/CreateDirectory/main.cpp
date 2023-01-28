d79a4c2ab495028bed5ac815908f28cacb5f3b98
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-01-28 14:02:14 +0900

    add CreateDirectory

---
+++ win32/CreateDirectory/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <string>
+
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+#include <commdlg.h>
+#include <shlwapi.h>
+#include <tchar.h>
+
+int main(int argc, char **argv)
+{
+    RemoveDirectoryA("MyDir1");
+    RemoveDirectoryW(L"MyDir2");
+    RemoveDirectory(TEXT("MyDir3"));
+
+    CreateDirectoryA("MyDir1", NULL);
+    CreateDirectoryW(L"MyDir2", NULL);
+    CreateDirectory(TEXT("MyDir3"), NULL);
+
+    assert(PathIsDirectoryA("MyDir1"));
+    assert(PathIsDirectoryW(L"MyDir2"));
+    assert(PathIsDirectory(TEXT("MyDir1")));
+
+    return 0;
+}
