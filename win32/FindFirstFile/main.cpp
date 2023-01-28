41a940a452b0c3e26b5ec8dda629f719f953a770
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-01-28 14:08:11 +0900

    add FindFirstFile

---
+++ win32/FindFirstFile/main.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+#include <commdlg.h>
+#include <shlwapi.h>
+#include <tchar.h>
+
+int main(int argc, char **argv)
+{
+    WIN32_FIND_DATAA find;
+    HANDLE hFind = FindFirstFileA("..\\*", &find);
+    if (hFind == INVALID_HANDLE_VALUE)
+    {
+        printf("Cannot found\n");
+        return 1;
+    }
+
+    do
+    {
+        if (find.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+        {
+            printf("dir: '%s'\n", find.cFileName);
+        }
+        else
+        {
+            printf("file: '%s'\n", find.cFileName);
+        }
+    } while (FindNextFile(hFind, &find));
+
+    FindClose(hFind);
+
+    return 0;
+}
