73aece156ec1c3338ff5b495b0cf8e501d9708a1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-01-28 13:50:53 +0900

    add MessageBox

---
+++ win32/MessageBox/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+#include <commdlg.h>
+#include <shlwapi.h>
+
+int main(int argc, char **argv)
+{
+    if (MessageBoxA(NULL, "Please press OK", "Hello", MB_OKCANCEL) == IDOK)
+    {
+        MessageBoxW(NULL, L"You pressed OK", L"Thank you!", MB_ICONINFORMATION);
+    }
+    else
+    {
+        MessageBox(NULL, TEXT("You pressed Cancel"), NULL, MB_ICONERROR);
+    }
+    return 0;
+}
