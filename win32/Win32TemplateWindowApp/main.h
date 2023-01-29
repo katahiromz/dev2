f5d8827189542a2a6a3bfe2ae13c8b063c5614a2
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-01-29 18:35:03 +0900

    str_...

--- win32/Win32TemplateWindowApp/main.h
+++ win32/Win32TemplateWindowApp/main.h
@@ -80,3 +80,98 @@ void DebugPrintfW(const wchar_t *fmt, ...);
     #define DPRINTA DebugPrintfA
     #define DPRINTW DebugPrintfW
 #endif
+
+inline void str_trim(LPSTR text, LPCSTR spaces = " \t\r\n")
+{
+    StrTrimA(text, spaces);
+}
+
+inline void str_trim(LPWSTR text, LPCWSTR spaces = L" \t\r\n\x3000")
+{
+    StrTrimW(text, spaces);
+}
+
+template <typename T_CHAR>
+inline void str_trim(std::basic_string<T_CHAR>& str, const T_CHAR *spaces)
+{
+    typedef std::basic_string<T_CHAR> string_type;
+    size_t i = str.find_first_not_of(spaces);
+    size_t j = str.find_last_not_of(spaces);
+    if ((i == string_type::npos) || (j == string_type::npos))
+    {
+        str.clear();
+    }
+    else
+    {
+        str = str.substr(i, j - i + 1);
+    }
+}
+
+template <typename T_STR>
+inline bool
+str_replace(T_STR& str, const T_STR& from, const T_STR& to)
+{
+    if (from.empty())
+        return false;
+
+    bool ret = false;
+    size_t i = 0;
+    for (;;) {
+        i = str.find(from, i);
+        if (i == T_STR::npos)
+            break;
+        ret = true;
+        str.replace(i, from.size(), to);
+        i += to.size();
+    }
+    return ret;
+}
+template <typename T_STR>
+inline bool
+str_replace(T_STR& str,
+            const typename T_STR::value_type *from,
+            const typename T_STR::value_type *to)
+{
+    assert(from != NULL);
+    assert(to != NULL);
+    return str_replace(str, T_STR(from), T_STR(to));
+}
+
+template <typename T_STR_CONTAINER>
+inline void
+str_split(T_STR_CONTAINER& container,
+          const typename T_STR_CONTAINER::value_type& str,
+          const typename T_STR_CONTAINER::value_type& chars)
+{
+    container.clear();
+    assert(!chars.empty());
+    size_t i = 0, k = str.find_first_of(chars);
+    while (k != T_STR_CONTAINER::value_type::npos)
+    {
+        container.push_back(str.substr(i, k - i));
+        i = k + 1;
+        k = str.find_first_of(chars, i);
+    }
+    container.push_back(str.substr(i));
+}
+
+template <typename T_STR_CONTAINER>
+inline typename T_STR_CONTAINER::value_type
+str_join(const T_STR_CONTAINER& container,
+         const typename T_STR_CONTAINER::value_type& sep)
+{
+    typename T_STR_CONTAINER::value_type result;
+    typename T_STR_CONTAINER::const_iterator it, end;
+    it = container.begin();
+    end = container.end();
+    if (it != end)
+    {
+        result = *it;
+        for (++it; it != end; ++it)
+        {
+            result += sep;
+            result += *it;
+        }
+    }
+    return result;
+}
